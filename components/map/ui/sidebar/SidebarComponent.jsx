edab2f49ae634cc13b045d5bdd9504ceba82be3b
Dqu1J <mcpeytwot5@gmail.com>
2023-06-19 21:33:54 +0100

    update styling of short desc

--- components/map/ui/sidebar/SidebarComponent.jsx
+++ components/map/ui/sidebar/SidebarComponent.jsx
@@ -3,7 +3,8 @@ import styles from './SidebarComponent.module.scss';
 export default function SidebarComponent({sidebarSettings, setSidebarSettings}) {
     return (
         <div className={`${styles.sidebar} ${sidebarSettings.displayed ? styles.shown : ""}`}>
-            <p dangerouslySetInnerHTML={{__html: sidebarSettings.shortDescription}}/>
+            <p className={styles.header} dangerouslySetInnerHTML={{__html: sidebarSettings.shortDescription}}/>
+
             <button onClick={() => {
                 setSidebarSettings({
                     displayed: false,
