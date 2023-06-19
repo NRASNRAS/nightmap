bc4400a612f558766be77a2d030a34bfbe7d37a1
Dqu1J <mcpeytwot5@gmail.com>
2023-06-19 22:08:06 +0100

    change image pos

--- components/map/ui/sidebar/SidebarComponent.jsx
+++ components/map/ui/sidebar/SidebarComponent.jsx
@@ -25,8 +25,10 @@ export default function SidebarComponent({sidebarSettings, setSidebarSettings}) 
 
             {data && (
                 <div className={styles.description}>
-                    {data.description}
                     <img src={baseDataUrl + data.image} alt=""/>
+                    <p>
+                        {data.description}
+                    </p>
                 </div>
             )}
 
