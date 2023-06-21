66ee36440ba2812ad2c6ea83bbd0318db72776b9
Dqu1J <mcpeytwot5@gmail.com>
2023-06-21 17:18:38 +0100

    better mobile support!

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }) {
                 <title>NightMap</title>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,1,-25"/>
                 <meta name='description' content='Night Map!!!'/>
+                <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
             </Head>
             <Component {...pageProps} />
         </>
