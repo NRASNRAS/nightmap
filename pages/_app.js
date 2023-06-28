94966c09c30a8c00277722c15d1e395aa6afeb72
Dqu1J <mcpeytwot5@gmail.com>
2023-06-28 04:26:48 +0100

    meta tags wtf

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,16 @@ export default function App({ Component, pageProps }) {
             <Head>
                 <title>NightMap</title>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,1,-25"/>
-                <meta name='description' content='Night Map!!!'/>
+
+                <link rel="icon" href="https://dynmap.nightrealm.net/live-atlas/favicons/favicon.svg"/>
+
+                <meta name='description' content='NightMap — A better alternative to dynmap for NightRealm'/>
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://nightmap.eservs.site/" />
+                <meta property="og:title" content="NightMap" />
+                <meta property="og:description" content="NightMap — A better alternative to dynmap for NightRealm" />
+                <meta name="theme-color" content="#202020"/>
+
                 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
             </Head>
             <Component {...pageProps} />
